a6e3386ba3d59bf5cc204b0264bb184759435049
Marius Rusu <marius.rusu@sendgrid.com>
2014-01-29 04:42:03 +0000

    default rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SendgridRailsSampleApp::Application.config.secret_key_base = 'ed7c819e9f08ffa2aca0d5c1089af198fa87355c96d1a36a3a53e661ccc07834f13a18226f0e039406d6b552b8be96179087f371730623d14d49b5eb00ad36ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SendgridRailsSampleApp::Application.config.session_store :cookie_store, key: '_sendgrid-rails-sample-app_session'
